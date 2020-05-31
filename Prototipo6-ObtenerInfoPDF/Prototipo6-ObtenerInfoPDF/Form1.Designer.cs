ec0415f1622fce2750c6e8d0977aea5ed6d5f98e
DenissePezoa <denisse.pezoa@gmail.com>
2020-05-31 02:12:25 -0400

    Agrega sincronización de marcadores, figuras (solo rectangulo) y comentarios

---
+++ Prototipo6-ObtenerInfoPDF/Prototipo6-ObtenerInfoPDF/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace Prototipo6_ObtenerInfoPDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.axAcroPDF1 = new AxAcroPDFLib.AxAcroPDF();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.vScrollBar2 = new System.Windows.Forms.VScrollBar();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(425, 449);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Sinc. Marcadores";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupBox1.Location = new System.Drawing.Point(149, 42);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(426, 346);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Aqui va documento";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(84, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(240, 320);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(556, 448);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 42);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Sinc. Comentarios";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(290, 449);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 41);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Sinc. Figuras";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // axAcroPDF1
+            // 
+            this.axAcroPDF1.Enabled = true;
+            this.axAcroPDF1.Location = new System.Drawing.Point(614, 42);
+            this.axAcroPDF1.Name = "axAcroPDF1";
+            this.axAcroPDF1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axAcroPDF1.OcxState")));
+            this.axAcroPDF1.Size = new System.Drawing.Size(359, 346);
+            this.axAcroPDF1.TabIndex = 4;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Location = new System.Drawing.Point(7, 371);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(139, 119);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Figuras";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 24);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(93, 22);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Ocultar/Mostrar Figuras";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.listBox2);
+            this.groupBox3.Controls.Add(this.vScrollBar1);
+            this.groupBox3.Location = new System.Drawing.Point(7, 42);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(137, 172);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Comentarios";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(6, 19);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(113, 147);
+            this.listBox2.TabIndex = 1;
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(119, 13);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(15, 156);
+            this.vScrollBar1.TabIndex = 0;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.listBox1);
+            this.groupBox4.Controls.Add(this.vScrollBar2);
+            this.groupBox4.Location = new System.Drawing.Point(7, 220);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(139, 145);
+            this.groupBox4.TabIndex = 7;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Marcadores";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 22);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(110, 121);
+            this.listBox1.TabIndex = 1;
+            // 
+            // vScrollBar2
+            // 
+            this.vScrollBar2.Location = new System.Drawing.Point(119, 16);
+            this.vScrollBar2.Name = "vScrollBar2";
+            this.vScrollBar2.Size = new System.Drawing.Size(20, 126);
+            this.vScrollBar2.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(340, 16);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(80, 122);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1155, 502);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.axAcroPDF1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Funcionalidades Digital a físico";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private AxAcroPDFLib.AxAcroPDF axAcroPDF1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.VScrollBar vScrollBar2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Prototipo6-ObtenerInfoPDF/Prototipo6-ObtenerInfoPDF/Form1.cs
@@ -0,0 +1,223 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prototipo6_ObtenerInfoPDF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string oldFile = "ejemploOK.pdf";
+            string newFile = "temporal.pdf";
+            PdfReader reader = new PdfReader(oldFile);
+            IList<Dictionary<String, Object>> bmProperties = SimpleBookmark.GetBookmark(reader);
+            PdfStamper stamp = new PdfStamper(reader, new FileStream(newFile, FileMode.Create));
+            if (bmProperties != null)
+            {
+                listBox1.Items.Clear();
+                MessageBox.Show("Cantidad marcadores " + bmProperties.Count());
+                foreach (IDictionary<String, Object> bmProperty in bmProperties)
+                {
+                    //MessageBox.Show("Key " + bmProperty.Keys + " y values " + bmProperty.Values);
+                    
+                    //bmProperty.TryGetValue("Page", out valor);
+                    
+                    //foreach (var algo in bmProperty.Values)
+                    //{
+                        
+                       /* if (algo == "Title")
+                        {
+                            listBox1.Items.Add(algo.);
+                        }
+                        if (algo == "Page")
+                        {
+                            listBox1.Items.Add(bmProperty.Values);
+                        }*/
+                        
+                        MessageBox.Show("pagina " + bmProperty["Page"] + " - Titulo "+ bmProperty["Title"]);
+                        var getPage = bmProperty["Page"];
+                        var getTittle = bmProperty["Title"];
+                       
+                        var aux_pagina =getPage.ToString().Split(' ');
+                        var pagina = aux_pagina[0];
+                        var titulo = getTittle;
+                        listBox1.Items.Add(titulo+ "  Pag. "+ pagina );
+                       
+                    //}
+                }
+                stamp.Close();
+                reader.Close();            }
+            else
+            {
+                MessageBox.Show("El documento No tiene marcadores / Se agregara uno a modo de prueba");
+                IList<Dictionary<String, Object>> marcadores = new List<Dictionary<String, Object>>();
+                
+                //marcadores.Add(marcador);
+                for (int i = 1; i <= 2; i++)
+                {
+                    Dictionary<String, Object> marcador = new Dictionary<String, Object>();
+                    marcador.Add("Action", "GoTo");
+                    marcador.Add("Title", "Marcador "+i);
+                    marcador.Add("Page", i+" XYZ 100 600 0");
+                    MessageBox.Show("marcador " + marcador.Count);
+                    marcadores.Add(marcador);
+                }
+              
+
+              
+
+                stamp.Outlines = marcadores;
+                stamp.Close();
+                //System.Diagnostics.Process.Start(newFile);
+                //Console.Read();
+                reader.Close();
+                File.Replace(newFile, oldFile, @"backup.pdf.bac");
+                MessageBox.Show("Pdf modificado con exito, se ha guardado un backup de la versión anterior ");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap default_image = new Bitmap(pictureBox1.Image);
+            Bitmap getIcono = new Bitmap("./noteIcon.png");
+           
+            Image<Bgr, Byte> fondo = new Image<Bgr, Byte>(default_image);
+            Image<Bgr, Byte> fondo2 = new Image<Bgr, Byte>(default_image);
+            Image <Bgr, Byte> icono = new Image<Bgr, Byte>(getIcono).Resize(0.8, Inter.Area);
+            //MessageBox.Show("tamano icono " + icono.Width + " y " + icono.Height);
+            string oldFile = "ejemploOK1.pdf";
+            string newFile = "temporal.pdf";
+            PdfReader reader = new PdfReader(oldFile);
+            PdfDictionary pageDict = reader.GetPageN(1);
+            
+            
+            
+
+            iTextSharp.text.Rectangle pagesize = reader.GetPageSize(1);
+            double anchoPDF = pagesize.Width;
+            double altoPDF = pagesize.Height;
+           // MessageBox.Show("Tamano pagina pdf --> ancho :" + anchoPDF + " y alto : " + altoPDF);
+            float anchoImagen = fondo.Width;
+            float altoImagen = fondo.Height;
+            //MessageBox.Show("Tamano imagen --> ancho :" + anchoImagen+ " y alto : " + altoImagen);
+
+            PdfArray annotArray = pageDict.GetAsArray(PdfName.ANNOTS);
+
+            for (int i = 0; i < annotArray.Size; ++i)
+            {
+                PdfDictionary curAnnot = annotArray.GetAsDict(i);
+                var subtype = curAnnot.Get(PdfName.SUBTYPE);
+                var rect = curAnnot.Get(PdfName.RECT);
+                var contents = curAnnot.Get(PdfName.CONTENTS);
+                var page = curAnnot.Get(PdfName.P);
+                //MessageBox.Show("Subtipo "+subtype + " coordenadas: "+rect + " y contenido "+contents);
+                if (subtype.ToString() == "/Square")
+                {
+                    
+                   
+                    
+                    //MessageBox.Show("Figura rectangular detectada coor "+rect);
+                    var aux_rect = rect.ToString().Split(',');
+                    aux_rect[0] = aux_rect[0].Remove(0, 1);
+                    aux_rect[3] = aux_rect[3].Remove( aux_rect[3].Length-1);
+                    aux_rect[0] = aux_rect[0].Replace(".", ",");
+                    aux_rect[1] = aux_rect[1].Replace(".", ",");
+                    aux_rect[2] = aux_rect[2].Replace(".", ",");
+                    aux_rect[3] = aux_rect[3].Replace(".", ",");
+                    //MessageBox.Show("el split primero " + aux_rect[0]+ " el split ultimo "+ aux_rect[3]);
+                   // MessageBox.Show("Esto es rect " + aux_rect[0] + " " + aux_rect[1] + " " + aux_rect[2] + " " + aux_rect[3]);
+                    int puntox1 = Convert.ToInt32(Convert.ToSingle(aux_rect[0]));
+                    int puntoy1 = Convert.ToInt32(Convert.ToSingle(aux_rect[1]));
+                    int puntox2 = Convert.ToInt32(Convert.ToSingle(aux_rect[2]));
+                    int puntoy2 = Convert.ToInt32(Convert.ToSingle(aux_rect[3]));
+                   // MessageBox.Show("puntos "+ puntox1+" ,"+ puntoy1+ " , "+ puntox2+" , "+ puntoy2);
+
+                    // TRANSFORMAR COORDENADAS DESDE PDF
+                    int nuevoX1 = (Convert.ToInt32(anchoImagen) * (puntox1 * 100 / Convert.ToInt32(anchoPDF))) / 100;
+                    int nuevoX2 = (Convert.ToInt32(anchoImagen) * (puntox2 * 100 / Convert.ToInt32(anchoPDF))) / 100;
+                    int nuevoY1 = Convert.ToInt32(altoImagen) - ((Convert.ToInt32(altoImagen) * (puntoy1 * 100 / Convert.ToInt32(altoPDF))) / 100);
+                    int nuevoY2 = Convert.ToInt32(altoImagen)-((Convert.ToInt32(altoImagen) * (puntoy2 * 100 / Convert.ToInt32(altoPDF))) / 100);
+                   // MessageBox.Show("puntos " + nuevoX1 + " ," + nuevoY1 + " , " + nuevoX2 + " , " + nuevoY2);
+
+                    Rectangle rectangulo = new Rectangle(puntox1, puntoy1, puntox1+puntox2, puntoy1+puntoy2);
+                    //Rectangle rectangulo = new Rectangle(0, 0, 100, 200);
+                    fondo.Draw(rectangulo, new Bgr(Color.Cyan), 1);
+                    Rectangle rectangulo2 = new Rectangle(nuevoX1, nuevoY1, nuevoX2-nuevoX1, nuevoY2-nuevoY1);
+                    //Rectangle rectangulo = new Rectangle(0, 0, 100, 200);
+                    fondo.Draw(rectangulo2, new Bgr(Color.Red), 1);
+                    pictureBox1.Image = fondo.Bitmap;
+                   // CvInvoke.cvResetImageROI(fondo);
+
+                }
+                if (subtype.ToString() == "/Text")
+                {
+                    //MessageBox.Show("Comentarios Post it detectado coor "+rect);
+                    listBox2.Items.Add(contents+" "+page);
+                    var aux_rect = rect.ToString().Split(',');
+                    aux_rect[0] = aux_rect[0].Remove(0, 1);
+                    aux_rect[0] = aux_rect[0].Replace(".", ",");
+                    aux_rect[1] = aux_rect[1].Replace(".", ",");
+                    int puntox1 = Convert.ToInt32(Convert.ToSingle(aux_rect[0]));
+                    int puntoy1 = Convert.ToInt32(Convert.ToSingle(aux_rect[1]));
+                    int nuevoX1 = (Convert.ToInt32(anchoImagen) * (puntox1 * 100 / Convert.ToInt32(anchoPDF))) / 100;
+                    int nuevoY1 = Convert.ToInt32(altoImagen) - ((Convert.ToInt32(altoImagen) * (puntoy1 * 100 / Convert.ToInt32(altoPDF))) / 100);
+                    //MessageBox.Show("Esto es punto x1 "+nuevoX1+ " y y1 "+nuevoY1);
+                    //ROi debe ser del mismo tamaño que la imagen a pegar
+                    Rectangle brect = new Rectangle(0, nuevoY1, icono.Width, icono.Height);
+                    fondo.ROI = brect;
+                    //final = auxImge.Copy();
+                    CvInvoke.cvCopy(icono, fondo, IntPtr.Zero);
+                    //pictureBox2.Image = fondo.Bitmap;
+
+
+                    CvInvoke.cvResetImageROI(fondo);
+                }
+                
+                //foreach (var algo in curAnnot.Keys)
+                //{
+
+                ////MessageBox.Show("Comentarios " + algo);
+                // listBox2.Items.Add(algo);
+                //}    
+                //int someType = MyCodeToGetAnAnnotsType(curAnnot);
+                //if (someType == THIS_TYPE)
+                //{
+
+                //}
+                //else if (someType == THAT_TYPE)
+                //{
+                //writeThatType(curAnnot);
+                //}
+            }
+        }
+        
+
+        public List<int> TransformarCoordenada(double anchoPDF, double altoPDF, int anchoImagen, int altoImagen, int puntox1, int puntoy1, int puntox2, int puntoy2)
+        {
+            List<int> nuevasCoor = new List<int>();
+            int nuevoX1 = (Convert.ToInt32(anchoImagen) * (puntox1 * 100 / Convert.ToInt32(anchoPDF))) / 100;
+            int nuevoX2 = (Convert.ToInt32(anchoImagen) * (puntox2 * 100 / Convert.ToInt32(anchoPDF))) / 100;
+            int nuevoY1 = (Convert.ToInt32(altoImagen) * (puntoy1 * 100 / Convert.ToInt32(altoPDF))) / 100;
+            int nuevoY2 = (Convert.ToInt32(altoImagen) * (puntoy1 * 100 / Convert.ToInt32(altoPDF))) / 100;
+            
+            return nuevasCoor;
+        }
+    }
+}
